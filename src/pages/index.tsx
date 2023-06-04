13b446241b2df7711b022a8b374a324b1c0a877c
messabih-khalil <msbih.khalil@gmail.com>
2023-06-04 08:10:28 +0100

    refactor(index.tsx): remove unused imports and replace Github Star link with a reusable component for better code organization and reusability

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
-import Image from 'next/image';
 import React from 'react';
-import { motion } from 'framer-motion';
-import Link from 'next/link';
 import RightBox from './components/RightBox';
 import LeftBox from './components/LeftBox';
 import Head from 'next/head';
+import GithubStart from '@/shared/GithubStart';
 const Home = () => {
+
   return (
     <>
       <Head>
@@ -18,26 +17,7 @@ const Home = () => {
         <RightBox />
 
         {/* Github Star */}
-        <Link
-          href="https://github.com/messabih-khalil/gitcards"
-          className="absolute  right-10 top-[50px]"
-        >
-          <motion.div
-            className="flex items-center border-b-[2px] border-yellow-400 pb-1 hover:scale-105"
-            initial={{
-              opacity: 0,
-            }}
-            animate={{
-              opacity: 1,
-              transition: {
-                duration: 0.2,
-              },
-            }}
-          >
-            <Image src="/assets/star.png" width={35} height={30} alt=""></Image>
-            <p className="ml-1 text-lg font-medium"> Give It A Star</p>
-          </motion.div>
-        </Link>
+        <GithubStart/>
       </section>
     </>
   );
